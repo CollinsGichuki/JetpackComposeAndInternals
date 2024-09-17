93c353f0ef8a7c8b2f7c652946fdf98c12e4a049
Collins Gichuki <collins.gichuki@bonial.com>
2024-09-17 21:37:58 +0200

    feat(SocialNetworkUser): implement solution for exercise 2

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SocialNetworkUser.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SocialNetworkUser.kt
@@ -1,8 +1,10 @@
 package dev.jorgecastillo.compose.app.ui.composables
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -14,13 +16,28 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun SocialNetworkUser(name: String, location: String, onFollow: () -> Unit) {
-    
+    Box(modifier = Modifier.fillMaxSize()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(modifier = Modifier.padding(12.dp)) {
+                Text(name)
+                Text(location)
+            }
+
+            Button(onClick = onFollow) {
+                Text("Follow")
+            }
+        }
+    }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun SocialNetworkUserPreview() {
-    SocialNetworkUser(name = "John Doe", location = "New York City") {
-        Log.d("Test", "Clicked!")
-    }
+    SocialNetworkUser(
+        name = "John Doe",
+        location = "New York City",
+        onFollow = {
+            Log.d("Test", "Clicked!")
+        }
+    )
 }
