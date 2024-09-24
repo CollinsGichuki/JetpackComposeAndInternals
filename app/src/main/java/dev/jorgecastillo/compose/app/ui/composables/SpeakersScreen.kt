a5d75ba135530190fa034ed3a0aaf15107d52283
CollinsGichuki <collinsgichuki9@gmail.com>
2024-09-24 17:15:29 +0200

    feat(SpeakerScreen): add shadow to composable

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SpeakersScreen.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SpeakersScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
